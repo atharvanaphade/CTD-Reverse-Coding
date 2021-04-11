871d406993f6110f872c21174a400cb39be4fa9b
Neil Deshpande <neildeshpande10@gmail.com>
2021-04-11 20:39:53 +0530

    Updated Users model according to functional requirements. Updated server timezone. Registered all models in admin panel.

--- Users/admin.py
+++ Users/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import (
+    Profile,
+    Question,
+    Submission,
+    TestCase,
+)
 
 # Register your models here.
+
+admin.site.register(Profile)
+admin.site.register(Question)
+admin.site.register(Submission)
+admin.site.register(TestCase)
--- Users/models.py
+++ Users/models.py
@@ -5,57 +5,54 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    college = models.CharField(max_length=255)
-    total_score = models.IntegerField(default=0)
-    junior = models.BooleanField(default=False)
-    correct_answered = models.IntegerField(default=0)
+    # college = models.CharField(max_length=255)
+    total_score = models.FloatField(default=0)
+    senior = models.BooleanField(default=False)
+    correct_answers = models.IntegerField(default=0)
     latest_submission_time = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.user.username
+        return ("profile_" + str(self.pk) + "_" + self.user.username)
+
 
 class Question(models.Model):
-    ques_title = models.CharField(max_length=255)
-    ques_desc = models.TextField("")
-    successful_attempts = models.IntegerField(default=0)
-    number_of_attempts = models.IntegerField(default=0)
-    score = models.IntegerField(default=0)
+    question_title = models.CharField(max_length=255)
+    question_desc = models.TextField("")
+    correct_attempts = models.IntegerField(default=0)
+    total_attempts = models.IntegerField(default=0)
+    max_marks = models.FloatField(default=0)
 
     def __str__(self):
-        return self.ques_title
+        return ("question_" + str(self.pk) + "_" + self.question_title)
 
-class Submissions(models.Model):
-    ques_id = models.ForeignKey(Question, on_delete=models.CASCADE)
+
+class Submission(models.Model):
+    user_id_fk = models.ForeignKey(User, on_delete=models.CASCADE)
+    question_id_fk = models.ForeignKey(Question, on_delete=models.CASCADE)
+    score = models.FloatField(default=0)
+    submission_time = models.DateTimeField(auto_now=True)
     attempt = models.IntegerField(default=0)
     status = models.CharField(default='NA', max_length=5)
-    submission_time = models.CharField(default="", max_length=15)
-    score = models.IntegerField(default=0)
     accuracy = models.FloatField(default=0)
 
-    class Meta:
-        verbose_name = 'Submissions'
-        verbose_name_plural = 'Submissions'
-    
     def __str__(self):
-        return(self.pk + " - question-" + str(self.ques_id.pk))
+        return("submission_" + str(self.pk) + "_" + self.user_id_fk.username + "_question_" + str(self.question_id_fk))
 
-class MultipleUserSubmission(models.Model):
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    submission_id = models.ForeignKey(Submissions, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return(self.user_id.username + "-" + self.submission_id)
 
 class TestCase(models.Model):
-    ques_id = models.ForeignKey(Question, on_delete=models.CASCADE)
-    time_limit = models.IntegerField(default=2)
-    mem_limit = models.IntegerField(default=64000000)
-    
+    question_id = models.ForeignKey(Question, on_delete=models.CASCADE)
 
-class Input(models.Model):
-    tc_id = models.ForeignKey(TestCase, on_delete=models.CASCADE)
-    input = models.TextField("")
+    # django supports json field for all databases
+    # it is stored as a string in the database but returned as a dictionary in a query
 
-class Output(models.Model):
-    tc_id = models.ForeignKey(TestCase, on_delete=models.CASCADE)
-    output = models.TextField("")
+    # input, output, time and memory limits are considered as sets stored in json format
+    # input set 1 will have multiple inputs
+    # there will be same number of output, time and memory limit entries for 1 test case
+
+    input = models.JSONField()
+    output = models.JSONField()
+    time_limit = models.JSONField()
+    memory_limit = models.JSONField()
+
+    def __str__(self):
+        return("test_case_" + str(self.pk) + "_question_" + self.question_id)
