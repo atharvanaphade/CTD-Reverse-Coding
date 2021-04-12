8461b89cc40c92bf8aa34ca1a7329650b92964a2
atharvanaphade <naphade21@gmail.com>
2021-04-13 00:03:01 +0530

    Added Auth

--- Users/urls.py
+++ Users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 from Users import views
+from rest_framework_simplejwt import views as jwt_views
 
 urlpatterns = [
     path('questions/', views.QuestionList.as_view()),
@@ -9,6 +10,8 @@ urlpatterns = [
     path('testcases/<int:pk>', views.TestCaseDetail.as_view()),
     path('profiles/', views.AccountList.as_view()),
     path('profiles/<int:pk>', views.AccountDetail.as_view()),
+    path('api/token/', jwt_views.TokenObtainPairView.as_view()),
+    path('api/token/refresh/', jwt_views.TokenRefreshView.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
